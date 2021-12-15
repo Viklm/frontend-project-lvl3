685b708e76c519598dffa7c8b412cb5b5cc543d5
Viktor <vik333444@yandex.ru>
2021-12-15 10:07:34 +0400

    added locales

---
+++ src/locales/ru.js
@@ -0,0 +1,18 @@
+export default {
+  translation: {
+    errors: {
+      invalid: 'Ссылка должна быть валидным URL',
+      duplicate: 'RSS уже существует',
+      network: 'Ошибка сети, попробуйте позже',
+      noContent: 'Ресурс не содержит валидный RSS',
+    },
+    successful: 'RSS успешно загружен',
+    posts: {
+      post: 'Посты',
+      button: 'Просмотр',
+    },
+    feeds: {
+      feed: 'Фиды',
+    },
+  },
+};
