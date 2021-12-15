59e05fac36b08494a07d2e78f4c2498e89a20d25
Viktor <vik333444@yandex.ru>
2021-12-15 13:07:20 +0400

    create update func and added feeds

--- src/app.js
+++ src/app.js
@@ -1,41 +1,88 @@
-import * as yup from 'yup';
+import axios from 'axios';
+import i18next from 'i18next';
 import watched from './view.js';
+import resources from './locales/index.js';
+import parser from './parser.js';
+import getRssPath from './routes.js';
+import validator from './validator.js';
 
 export default () => {
-  const state = {
-    links: [],
-    form: {
-      validationState: 'valid', // valid, invalid, duplicate
-      process: 'filling', // filling, failed, processed, successful
-      processError: null,
-      error: '',
-      url: '',
-    },
-  };
-  const watchedState = watched(state);
-  const schema = yup.object().shape({
-    website: yup.string().url(),
-  });
-  const form = document.querySelector('form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const value = formData.get('url');
-    schema.validate({ website: value }).then(() => {
-      if (!watchedState.links.includes(value)) {
-        watchedState.links.unshift(value);
-        watchedState.form.validationState = 'valid';
-      } else {
-        watchedState.form.validationState = 'duplicate';
-        watchedState.form.erorr = 'RSS уже существует';
-        watchedState.form.process = 'failed';
-      }
-    }).catch(() => {
-      watchedState.form.validationState = 'invalid';
-      watchedState.form.erorr = 'Ссылка должна быть валидным URL';
-      watchedState.form.process = 'failed';
+  const i18nextInstance = i18next.createInstance();
+  return i18nextInstance.init({
+    lng: 'ru',
+    debug: true,
+    resources,
+  }).then(() => {
+    const state = {
+      links: [],
+      feeds: [],
+      posts: [],
+      countOfCall: 0,
+      readPosts: [],
+      id: {
+        post: 0,
+        fid: 0,
+      },
+      form: {
+        update: null, // loading, loaded
+        validationState: 'valid', // valid, invalid, duplicate
+        process: 'filling', // filling, failed, processed, successful
+        processError: null,
+        error: '',
+      },
+    };
+
+    const watchedState = watched(state, i18nextInstance);
+
+    const updatePosts = () => {
+      setTimeout(() => {
+        const request = (path) => axios.get(getRssPath(path))
+          .then((response) => parser(response.data.contents))
+          .then(({ posts }) => posts);
+        Promise.all(watchedState.links.map((link) => request(link)))
+          .then((data) => {
+            const result = data.flat();
+            watchedState.posts.unshift(...result);
+            watchedState.form.update = 'loaded';
+            updatePosts();
+          });
+      }, 5000);
+    };
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const value = formData.get('url');
+      validator(value, i18nextInstance).then(() => {
+        if (!watchedState.links.includes(value)) {
+          watchedState.links.unshift(value);
+          watchedState.form.validationState = 'valid';
+        } else {
+          watchedState.form.validationState = 'duplicate';
+        }
+      }).catch(() => {
+        watchedState.form.validationState = 'invalid';
+      }).then(() => {
+        if (watchedState.form.validationState === 'valid') {
+          watchedState.form.process = 'processed';
+          axios.get(getRssPath(value))
+            .then((response) => {
+              const data = parser(response.data.contents);
+              const { feed, posts } = data;
+              watchedState.feeds.unshift(feed);
+              watchedState.posts.unshift(...posts);
+              watchedState.form.process = 'successful';
+              watchedState.form.update = 'loading';
+              watchedState.countOfCall += 1;
+              if (watchedState.countOfCall < 2) {
+                updatePosts();
+              }
+            });
+        }
+      });
+      form.reset();
+      document.getElementById('url-input').focus();
     });
-    form.reset();
-    document.getElementById('url-input').focus();
   });
 };
--- src/view.js
+++ src/view.js
@@ -1,31 +1,60 @@
 import onChange from 'on-change';
+import render from './render.js';
 
-const watchedState = (state) => onChange(state, (path, value) => {
+const watchedState = (state, text) => onChange(state, (path, value) => {
   const feedback = document.querySelector('p.feedback');
   const input = document.getElementById('url-input');
-  // const form = document.querySelector('form');
-  // const button = document.querySelector('[aria-label="add"]');
+  const button = document.querySelector('[aria-label="add"]');
+  const divOfFeeds = document.querySelector('.feeds');
+  const divOfPosts = document.querySelector('.posts');
+
+  const disable = (inputEl, buttonEl) => {
+    inputEl.setAttribute('readonly', true);
+    buttonEl.setAttribute('disabled', true);
+  };
+  const enable = (inputEl, buttonEl) => {
+    inputEl.removeAttribute('readonly');
+    buttonEl.removeAttribute('disabled');
+  };
 
   if (path === 'form.validationState') {
     if (value === 'invalid') {
-      input.classList.add('is-invalid');
-      feedback.classList.add('text-danger');
-      feedback.classList.remove('text-success');
-      feedback.textContent = 'Ссылка должна быть валидным URL';
+      render.erorrs(input, feedback, text.t('errors.invalid'));
     }
     if (value === 'duplicate') {
-      input.classList.add('is-invalid');
-      feedback.classList.add('text-danger');
-      feedback.classList.remove('text-success');
-      feedback.textContent = 'RSS уже существует';
+      render.erorrs(input, feedback, text.t('errors.duplicate'));
     }
   }
-  // if (path === 'form.process') {
-  //   if (value === 'failed') {
-  //     input.setAttribute('readonly', true);
-  //     button.setAttribute('disabled', true);
-  //   }
-  // }
+
+  if (path === 'form.process') {
+    switch (value) {
+      case 'processed':
+        feedback.textContent = '';
+        disable(input, button);
+        break;
+      case 'failed':
+        enable(input, button);
+        render.erorrs(input, feedback, text.t('errors.network'));
+        break;
+      case 'filling':
+        enable(input, button);
+        break;
+      case 'successful':
+        enable(input, button);
+        render.successFeedback(input, feedback, text);
+        render.feeds(divOfFeeds, text, state);
+        render.posts(divOfPosts, text, state);
+        break;
+      default:
+        throw new Error(`Unknow status ${value}`);
+    }
+  }
+
+  if (path === 'form.update') {
+    if (value === 'loaded') {
+      render.posts(divOfPosts, text, state);
+    }
+  }
 });
 
 export default watchedState;
